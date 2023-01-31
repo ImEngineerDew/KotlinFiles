b56ead547e4716eafe2181bc38b71f7871c1a7c8
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-30 22:11:49 -0500

    Prior to launch the problem 001

--- Challenges/Testing/src/main/kotlin/Main.kt
+++ Challenges/Testing/src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 import java.util.Scanner;
 
+fun sumAplusB (a:Int, b: Int): Int
+{
+    return a+b;
+}
+
 fun main(args: Array<String>)
 {
     val obj_var = Scanner (System.`in`);
 
-    print("Write your text: ");
-    val variable_name = obj_var.nextLine();
+    print("Write your value of A: ");
+    val valA = obj_var.nextInt();
 
-    print("Text: "+variable_name);
+    print("Write your valur of B: ");
+    val valB = obj_var.nextInt();
+
+    val answer = sumAplusB(valA,valB);
+
+    print(answer);
 
 }
