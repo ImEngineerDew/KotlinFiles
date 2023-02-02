507f5156e12ed08f9feb1e0c88c8401283a63e84
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-02 16:48:02 -0500

    Putting the first for and comments prior to solve#

--- Challenges/project003/src/main/kotlin/Main.kt
+++ Challenges/project003/src/main/kotlin/Main.kt
@@ -4,6 +4,21 @@ fun main() {
 
     val entrance = Scanner(System.`in`)
 
-    println("Write the size of array: ")
+    print("Write the size of array: ")
     val length = entrance.nextInt()
+
+    /**Java equivalent to Integer sumA[] = new Integer["length"]**/
+    val sumA = arrayOfNulls<Int>(length)
+
+    /**Java equivalent to Integer sumB[] = new Integer["length"]**/
+    val sumB = arrayOfNulls<Int>(length)
+
+    /**Java equivalent to Integer sumC[] = new Integer["length"]**/
+    val sumC = arrayOfNulls<Int>(length)
+
+    for (i in sumA.indices) {
+
+        sumA[i] = entrance.nextInt()
+        sumB[i] = entrance.nextInt()
+    }
 }
