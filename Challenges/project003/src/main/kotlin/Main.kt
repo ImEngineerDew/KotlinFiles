c59596d2182a381e386d192530d119d310cad64a
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-02 16:42:05 -0500

    Erased the method inside of fun main class, and introduce the Scanner class

--- Challenges/project003/src/main/kotlin/Main.kt
+++ Challenges/project003/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
-fun main(args: Array<String>) {
+import java.util.Scanner;
 
+fun main() {
+
+    val entrance = Scanner(System.`in`)
+
+    println("Write the size of array: ")
+    val length = entrance.nextInt()
 }
