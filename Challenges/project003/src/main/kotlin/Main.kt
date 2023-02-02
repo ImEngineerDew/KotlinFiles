1ab099b0b2b9f1d1d5d4d7537dbb054978afa069
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-02 16:57:07 -0500

    Problem 003 has been solved

--- Challenges/project003/src/main/kotlin/Main.kt
+++ Challenges/project003/src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
-import java.util.Scanner;
+import java.util.Scanner
 
 fun main() {
 
     val entrance = Scanner(System.`in`)
 
     print("Write the size of array: ")
-    val length = entrance.nextInt()
+    val tam = entrance.nextInt()
 
     /**Java equivalent to Integer sumA[] = new Integer["length"]**/
-    val sumA = arrayOfNulls<Int>(length)
+    val sumA = arrayOfNulls<Int>(tam)
 
     /**Java equivalent to Integer sumB[] = new Integer["length"]**/
-    val sumB = arrayOfNulls<Int>(length)
+    val sumB = arrayOfNulls<Int>(tam)
 
     /**Java equivalent to Integer sumC[] = new Integer["length"]**/
-    val resu = arrayOfNulls<Int>(length)
+    val resu = arrayOfNulls<Int>(tam)
 
     for (i in sumA.indices) {
         print("Write the values of A: ")
@@ -23,5 +23,10 @@ fun main() {
         print("Write the values of B: ")
         sumB[i] = entrance.nextInt()
     }
-    
+    for (j in resu.indices) {
+        resu[j] = sumA[j]!! + sumB[j]!!
+        print(" "+resu[j]!!)
+    }
 }
+
+
