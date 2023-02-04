664245e768f2948aead64e8e9d32ee2d6ad06823
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-04 10:03:01 -0500

    Writing an useful for exercise

---
+++ RandomFiles/ran001/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main() {
+
+    /**This line code is the input data**/
+    val entrance = Scanner(System.`in`)
+
+    print("Define the size of you array: ")
+    val size = entrance.nextInt()
+
+    val numArray = arrayOfNulls<Int>(size)
+
+    print("Write the numbers: ")
+    val numbers = entrance.nextInt()
+
+}
