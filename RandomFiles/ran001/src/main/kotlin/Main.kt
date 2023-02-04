1334f20dcf1ec040e4b444cf38d29b4d699a19f1
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-04 11:46:52 -0500

    Some new features has been introduced on random file

--- RandomFiles/ran001/src/main/kotlin/Main.kt
+++ RandomFiles/ran001/src/main/kotlin/Main.kt
@@ -9,12 +9,13 @@ fun main() {
     val size = entrance.nextInt()
 
     val numArray = arrayOfNulls<Int>(size)
+    var numbers = 0
 
     for(index in numArray.indices) {
         print("Write the numbers: ")
-        val numbers = entrance.nextInt()
-
-        numArray[index] = numbers
+        numArray[index] = entrance.nextInt()
+        numbers = numbers.plus(numArray[index]!!)
     }
-    print(Arrays.toString(numArray))
+    print("Values of array: "+ numArray.contentToString())
+    println("Result of the sum of this array: $numbers")
 }
