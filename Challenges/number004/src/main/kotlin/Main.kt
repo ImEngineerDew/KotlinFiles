165a6382bc8e0dd24109a7067d7b2e90d6bf3451
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-05 00:25:39 -0500

    Problem 004 has been solved

--- Challenges/number004/src/main/kotlin/Main.kt
+++ Challenges/number004/src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import java.util.*
-
 fun main() {
 
     val input = Scanner(System.`in`)
@@ -10,7 +9,9 @@ fun main() {
     println(" ")
     val arrayA = arrayOfNulls<Int>(sizeArray)
     val arrayB = arrayOfNulls<Int>(sizeArray)
-    val arrayC = arrayOfNulls<Int>(sizeArray)
+    val arrayComp = arrayOfNulls<Int>(sizeArray)
+
+    var index = 0
 
     for (index in arrayA.indices) {
         print("Write the numbers of arrayA: ")
@@ -20,14 +21,15 @@ fun main() {
     for (index in arrayB.indices) {
         print("Write the numbers of arrayB: ")
         arrayB[index] = input.nextInt()
-    }
-    println(" ")
-    for (index in arrayC.indices) {
-        print("Write the numbers of arrayC: ")
-        arrayC[index] = input.nextInt()
+
+        if (arrayA[index]!!.compareTo(arrayB[index]!!) < 0) {
+            arrayComp[index] = arrayA[index]!!
+        } else if (arrayA[index]!!.compareTo(arrayB[index]!!) > 0) {
+            arrayComp[index] = arrayB[index]!!
+        }
     }
     println(" ")
     println("Numbers of array A: " + Arrays.toString(arrayA))
     println("Numbers of array B: " + Arrays.toString(arrayB))
-    println("Numbers of array C: " + Arrays.toString(arrayC))
+    println("Numbers of compared arrays: " + Arrays.toString(arrayComp))
 }
