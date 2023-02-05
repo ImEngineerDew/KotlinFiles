4f5479294bba5af3b06eec41a70f4dd99dd55a0f
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-04 23:54:57 -0500

    Creating the solution about of problem004

---
+++ Challenges/number004/untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    val input = Scanner(System.`in`)
+    val sizeArray = input.nextInt()
+
+    val arrayA = arrayOfNulls<Int>(sizeArray)
+    val arrayB = arrayOfNulls<Int>(sizeArray)
+    val arrayC = arrayOfNulls<Int>(sizeArray)
+
+    
+
+}
