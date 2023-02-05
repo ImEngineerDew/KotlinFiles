c57ce428a0df78982d48cc90d30a9bf00d0ab6d6
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-05 08:49:38 -0500

    Solving the problem005, writing the Scanner class and their inputs

--- Challenges/number005/src/main/kotlin/Main.kt
+++ Challenges/number005/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
-fun main(args: Array<String>) {
+import java.util.Scanner
+fun main(/**args: Array<String>**/) {
 
+    val input = Scanner(System.`in`)
+
+    print("Please write the size of your array: ")
+    val sizeArray = input.nextInt()
+    
 }
