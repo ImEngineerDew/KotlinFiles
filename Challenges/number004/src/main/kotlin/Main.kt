6026428c8b0ccc2dff7b06633bf43918af87a6e4
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-05 00:06:38 -0500

    Some changes has been introduced on problem 004

--- Challenges/number004/src/main/kotlin/Main.kt
+++ Challenges/number004/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-fun main(args: Array<String>) {
+fun main() {
 
     val input = Scanner(System.`in`)
 
@@ -11,22 +11,21 @@ fun main(args: Array<String>) {
     val arrayB = arrayOfNulls<Int>(sizeArray)
     val arrayC = arrayOfNulls<Int>(sizeArray)
 
-    for(index in arrayA.indices) {
+    for (index in arrayA.indices) {
         print("Write the numbers of arrayA: ")
-        arrayA[index]= input.nextInt()
+        arrayA[index] = input.nextInt()
     }
-    for(index in arrayB.indices) {
+    print(" ")
+    for (index in arrayB.indices) {
         print("Write the numbers of arrayB: ")
         arrayB[index] = input.nextInt()
     }
-    for(index in arrayC.indices)
-    {
+    print(" ")
+    for (index in arrayC.indices) {
         print("Write the numbers of arrayC: ")
         arrayC[index] = input.nextInt()
     }
-    println("Numbers of array A: "+Arrays.toString(arrayA))
-    println("Numbers of array B: "+Arrays.toString(arrayB))
-    println("Numbers of array C: "+Arrays.toString(arrayC))
-
-
+    println("Numbers of array A: " + Arrays.toString(arrayA))
+    println("Numbers of array B: " + Arrays.toString(arrayB))
+    println("Numbers of array C: " + Arrays.toString(arrayC))
 }
