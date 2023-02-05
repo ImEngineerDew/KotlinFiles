2e5e91ad1bfac8710d2815ad010b8c78ffd42f42
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-04 23:58:25 -0500

    Continue with the code ...

--- Challenges/number004/src/main/kotlin/Main.kt
+++ Challenges/number004/src/main/kotlin/Main.kt
@@ -3,11 +3,19 @@ import java.util.Scanner
 fun main(args: Array<String>) {
 
     val input = Scanner(System.`in`)
+
+    print("Write the size of array: ")
     val sizeArray = input.nextInt()
 
     val arrayA = arrayOfNulls<Int>(sizeArray)
     val arrayB = arrayOfNulls<Int>(sizeArray)
     val arrayC = arrayOfNulls<Int>(sizeArray)
+
+    for(index in arrayA.indices) {
+        print("Write the numbers of arrayA: ")
+        arrayA[index]= input.nextInt()
+
+    }
 
 
 
