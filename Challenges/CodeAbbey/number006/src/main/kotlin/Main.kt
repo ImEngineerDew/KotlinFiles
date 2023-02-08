fb1b6ce6e64157d7bd9d4405057cb014b25c0d3d
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-07 20:26:50 -0500

    Putting the for sentences

--- Challenges/CodeAbbey/number006/src/main/kotlin/Main.kt
+++ Challenges/CodeAbbey/number006/src/main/kotlin/Main.kt
@@ -1,4 +1,27 @@
 import java.util.Scanner
-fun main(args: Array<String>) {
+
+fun divisorOperation(a: Int, b: Int): Double  {
+    return a.toDouble()/b.toDouble()
+}
+
+fun main() {
+
+    val obj = Scanner(System.`in`)
+
+    print("Write the size of array: ")
+    val size = obj.nextInt()
+
+    val vectorA = arrayOfNulls<Int>(size)
+    val vectorB = arrayOfNulls<Int>(size)
+    val vectorC = arrayOfNulls<Int>(size)
+
+    for(index in vectorA.indices) {
+        print("Write the values of vector A: ")
+        vectorA[index] = obj.nextInt()
+    }
+    for(index in vectorB.indices) {
+        print("Write the values of vector B: ")
+        vectorB[index] = obj.nextInt()
+    }
 
 }
