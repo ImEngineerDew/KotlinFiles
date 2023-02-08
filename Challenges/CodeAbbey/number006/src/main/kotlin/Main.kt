af9146c798cb9f69eadaf9409820c065c5c3fc42
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-07 21:29:53 -0500

    Working to problem 006

--- Challenges/CodeAbbey/number006/src/main/kotlin/Main.kt
+++ Challenges/CodeAbbey/number006/src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 import java.util.Scanner
 
-fun divisorOperation(a: Int, b: Int): Double  {
-    return a.toDouble()/b.toDouble()
+fun divisorOperation(a: Int, b: Int): Double {
+    return a.toDouble() / b.toDouble()
 }
 
 fun main() {
-
     val obj = Scanner(System.`in`)
 
     print("Write the size of array: ")
@@ -15,11 +14,11 @@ fun main() {
     val vectorB = arrayOfNulls<Int>(size)
     val vectorC = arrayOfNulls<Int>(size)
 
-    for(index in vectorA.indices) {
+    for (index in vectorA.indices) {
         print("Write the values of vector A: ")
         vectorA[index] = obj.nextInt()
     }
-    for(index in vectorB.indices) {
+    for (index in vectorB.indices) {
         print("Write the values of vector B: ")
         vectorB[index] = obj.nextInt()
     }
