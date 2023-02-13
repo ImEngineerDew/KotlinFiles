eaec4794253bf58128abf941b254ff2c58641ffe
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-13 17:20:42 -0500

    Create the variables for the problem 009

--- Challenges/CodeAbbey/number009/src/main/kotlin/Main.kt
+++ Challenges/CodeAbbey/number009/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val obj = Scanner(System.`in`)
+    val valueArr = obj.nextInt()
+
+    val sideA = arrayOfNulls<Int>(valueArr)
+    val sideB = arrayOfNulls<Int>(valueArr)
+    val sideC = arrayOfNulls<Int>(valueArr)
+    
+
 }
