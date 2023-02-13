166d2b1022279baae4e981f9bd53c04c9cd320a2
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-13 17:28:27 -0500

    Problem 009 has been solved, again!

--- Challenges/CodeAbbey/number009/src/main/kotlin/Main.kt
+++ Challenges/CodeAbbey/number009/src/main/kotlin/Main.kt
@@ -8,6 +8,27 @@ fun main(args: Array<String>) {
     val sideA = arrayOfNulls<Int>(valueArr)
     val sideB = arrayOfNulls<Int>(valueArr)
     val sideC = arrayOfNulls<Int>(valueArr)
-    
 
+    val triangle = arrayOfNulls<Int>(valueArr)
+
+    for (index in sideA.indices) {
+        sideA[index] = obj.nextInt()
+        sideB[index] = obj.nextInt()
+        sideC[index] = obj.nextInt()
+    }
+    for (index in triangle.indices) {
+        if ((sideA[index]!! + sideB[index]!!) > sideC[index]!!) {
+            if ((sideA[index]!! + sideC[index]!!) > sideB[index]!!) {
+                if ((sideC[index]!! + sideB[index]!!) > sideA[index]!!) {
+                    print(1)
+                } else {
+                    print(0)
+                }
+            } else {
+                print(0)
+            }
+        } else {
+            print(0)
+        }
+    }
 }
